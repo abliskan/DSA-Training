a0e0900b875fd81d8a313f33320a0ffdf4dc8e57
RickyS <suhanryricky@yahoo.com>
2022-09-19 15:06:13 +0700

    hackerrank python exercise 2-star #2

---
+++ Python/hackerrank/2-star/Nested_lists.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    l = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        l.append([name, score])
+    [print(names) for names in sorted([name for name,
+     tests in l if tests == 
+     sorted(list(set([tests for name, tests in l])))[1]])]
