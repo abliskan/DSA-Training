47276c50cfa8a0c1bf1a48e9cf77cc130a0c0d7a
RickyS <35142826+abliskan@users.noreply.github.com>
2022-09-26 10:23:33 +0700

    hackerrank python exercise 3-star #2

--- Python/hackerrank/3-star/Set-Mutations.py
+++ Python/hackerrank/3-star/Set-Mutations.py
@@ -1,6 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 x = int(input())
 temp = set(map(int,input().split()))
+
 for _ in range(int(input())):
     y = input().split()
     getattr(temp,y[0])(set(map(int,input().split())))
