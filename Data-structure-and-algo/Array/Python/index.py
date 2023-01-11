14023fb64597b7beac7a452754135c18d9cd5df3
RickyS <suhanryricky@yahoo.com>
2023-01-11 22:17:33 +0700

    insert DS+A folder

---
+++ Data-structure-and-algo/Array/Python/index.py
@@ -0,0 +1,52 @@
+import array
+
+print("******************* Array")
+# initializing array with array values
+# initializes array with signed integers
+arr = array.array('i', [1, 2, 3, 5, 7])
+
+# printing original array
+print("The new created array is : ", end=" ")
+for i in range(0, 3):
+    print(arr[i], end=" ")
+
+print("\r")
+
+# using append() to insert new value at end
+arr.append(4);
+
+# printing appended array
+print("The appended array is : ", end="")
+for i in range(0, 4):
+    print(arr[i], end=" ")
+
+# using insert() to insert value at specific position
+# inserts 5 at 2nd position
+arr.insert(2, 5)
+
+print("\r")
+# printing array after insertion
+print("The array after insertion is : ", end="")
+for i in range(0, 5):
+    print(arr[i], end=" ")
+
+print("\r")
+# using pop() to remove element at 2nd position
+print("The popped element is : ", end="")
+print(arr.pop(2));
+
+# printing array after popping
+print("The array after popping is : ", end="")
+for i in range(0, 4):
+    print(arr[i], end=" ")
+
+print("\r")
+
+# using remove() to remove 1st occurrence of 1
+arr.remove(1)
+
+# printing array after removing
+print("The array after removing is : ", end="")
+for i in range(0, 3):
+    print(arr[i], end=" ")
+print("\n")
