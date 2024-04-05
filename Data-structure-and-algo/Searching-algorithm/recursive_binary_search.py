c5e7fde5b363dff58b95a261883403b507df0eb6
RickyS <suhanryricky@yahoo.com>
2024-04-05 20:04:39 +0700

    2024-series-day2: search-algorithm-pt2

---
+++ Data-structure-and-algo/Searching-algorithm/recursive_binary_search.py
@@ -0,0 +1,21 @@
+def recursive_binary_search(arr, first_index, last_index, target):
+    if last_index >= first_index:
+        middle_index = first_index + last_index - 1 // 2
+        # middle_element = arr[middle_index]
+        # arr_index = len(arr)
+        # print(f"Current middle element {middle_element} occurs in position {middle_index}")
+        if target > arr[middle_index]:
+            recursive_binary_search(arr, middle_index + 1, last_index, target)
+        elif target < arr[middle_index]:
+            return recursive_binary_search(arr, first_index, last_index - 1, target)
+        else:
+            return middle_index
+    else:
+        return -1
+
+
+array = [2, 5, 8, 10, 16, 22, 25]
+# array = [8, 10, 16, 22, 25, 32]
+target = 10
+search_target = recursive_binary_search(array, 0, len(array) - 1, target)
+print(f"Element you search {target} is present at index {search_target}")
