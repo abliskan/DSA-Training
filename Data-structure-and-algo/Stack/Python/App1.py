aa33f76e73e7603899044b8647477bedc5dbe43c
RickyS <suhanryricky@yahoo.com>
2024-05-29 20:46:40 +0700

    2024-series-day-12: stack-push-ops-1

--- Data-structure-and-algo/Stack/Python/App1.py
+++ Data-structure-and-algo/Stack/Python/App1.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def peek(self):
+        if len(self) == 0:
+            raise Exception("Stack is empty right know")
+        return self.stack[0]
+
+
+if __name__ == '__main__':
+    stack1 = Stack()
+    stack1.push(1)
+    stack1.push(3)
+    stack1.push(5)
+    stack1.push(4)
+    print("Current element on stack: " + str(stack1.stack))
