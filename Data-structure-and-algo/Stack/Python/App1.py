7ccb1f532cd73f47b066a237b7c93d58c9b4f0a7
RickyS <suhanryricky@yahoo.com>
2024-05-30 20:35:08 +0700

    2024-series-day-12: stack-isEmpty-peek-size-ops-2

--- Data-structure-and-algo/Stack/Python/App1.py
+++ Data-structure-and-algo/Stack/Python/App1.py
@@ -5,6 +5,12 @@ class Stack:
     def push(self, item):
         self.stack.append(item)
 
+    def is_empty(self):
+        return self.stack == []
+
+    def stack_size(self):
+        return len(self.stack)
+
     def peek(self):
         if len(self) == 0:
             raise Exception("Stack is empty right know")
@@ -18,3 +24,5 @@ if __name__ == '__main__':
     stack1.push(5)
     stack1.push(4)
     print("Current element on stack: " + str(stack1.stack))
+    stack1.is_empty()
+    print("Size of the stack:", stack1.stack_size())
