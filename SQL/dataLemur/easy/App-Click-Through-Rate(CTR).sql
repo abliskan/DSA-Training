083c5a5d1df700f605c11a8a45e233edbf58d6ca
RickyS <suhanryricky@yahoo.com>
2024-12-23 15:11:20 +0700

    Add create new solution for easy sql exercise #11

---
+++ SQL/dataLemur/easy/App-Click-Through-Rate(CTR).sql
@@ -0,0 +1,9 @@
+-- Sample num-1 | common SQL --
+SELECT app_id,
+       ROUND((100.0 * 
+        SUM(CASE WHEN event_type = 'click' THEN 1 ELSE 0 END) /
+        SUM(CASE WHEN event_type = 'impression' THEN 1 ELSE 0 END)
+       ), 2) AS ctr
+FROM events
+WHERE EXTRACT(YEAR from timestamp) = '2022'
+GROUP BY app_id;
